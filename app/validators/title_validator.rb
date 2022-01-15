97979cf7ed32b8d1236ccf6a3a75b1b2687be352
github username <kelly.hesterly@gmail.com>
2022-01-14 23:19:23 -0700

    commit tree 376d44121dff8eb55062c28d7ffb4a003f8254c4

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,9 +1,9 @@
 class TitleValidator < ActiveModel::Validator
     def validate(record)
-        binding.pry
-        unless record.title.include?("Won't Believe" || "Secret" || "Top" || "Guess" )
-          
-            record.errors[:title] << "Title must include clickbait word"
+        clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
+            
+        unless record.title.to_s.include? "Won't Believe" || "Secret" || "Top" || "Guess"
+            record.errors[:title] << "The title needs to include clickbait!"
         end
     end
 end
